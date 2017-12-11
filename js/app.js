30ca09b6834fa1fa5b90c504bc07428a497613c8
Görkem SARI <gorkem.sari@gmail.com>
2017-12-11 18:12:46 +0300

    jsPDF test

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ $( document ).ready(function() {
     });
 });
 
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function () {
+    doc.fromHTML($('.container').html(), 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+    });
+    doc.save('sample-file.pdf');
+});
